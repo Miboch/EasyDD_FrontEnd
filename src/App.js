bcf7074ed8f4416daa5f8081eebee230404d308a
Ark <twitcharkraga@gmail.com>
2021-04-27 14:36:22 -0500

    We added Daddyfont

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Nav-bar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-5xl">Hi</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Nav-bar/NavBar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Im the nav</h1>
+    </div>
+  );
+};
+
+export default Navbar;
