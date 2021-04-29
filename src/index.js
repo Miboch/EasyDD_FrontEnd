185326a131c999a7e21b8879f26669270f2058c7
Ark <twitcharkraga@gmail.com>
2021-04-29 12:25:31 -0500

    route/componet page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import { BrowserRouter as Router } from "react-router-dom";
 const root = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   root
 );
