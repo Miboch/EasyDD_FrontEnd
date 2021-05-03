b39604bcc9aa9afb99eb1d896527164bb782ab92
Ark <twitcharkraga@gmail.com>
2021-05-03 12:42:29 -0500

    Route and base componet set up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Navbar from "./components/Nav-bar/NavBar";
 import { Route, Link, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
+import SignUp from "./components/Signup/SignUp";
 
 function App() {
   return (
     <div className="App bg-appColor">
       <Navbar />
       <Switch>
-        <Route path="/login" render={(props) => <Login />} />
+        <Route exact path="/" render={(props) => <Login />} />
+        <Route path="/signup" render={(props) => <SignUp />} />
       </Switch>
     </div>
   );
